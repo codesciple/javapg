61d3271bb77a757b3e482d8b552510e147a87bff
codesciple <codesciple@gmail.com>
2021-02-27 22:41:59 +0530

    codeCademy-challenge

---
+++ Restaurant.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Restaurant {
+
+    public static void main(String[] args) {
+
+        String[] groceryItems = {"steak", "milk", "jelly beans"};
+        double[] prices = {25.00, 2.95, 2.50};
+
+        // Adding ham to the groceries
+        System.out.println(prices);
+
+    }
+
+}
